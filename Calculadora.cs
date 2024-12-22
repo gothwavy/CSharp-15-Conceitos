dcc32e6fc21d4a4183a18b1d16dd0a8987c20726
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:19:49 -0300

    Finalizado✅

---
+++ Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Interface
+{
+    internal class Calculadora : ICalculadoraBase, ICalculadoraAvancada
+    {
+        public double Cosseno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divisao()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiplicacao()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Seno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Soma()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtracao()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Tangente()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _15_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // As interfaces possuem apenas metodos obrigatorios, e por padrao no C#, todos os nomes de interface comecam com a letra "i".
+
+        }
+    }
+}
